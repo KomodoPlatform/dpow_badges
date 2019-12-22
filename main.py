89789c83749b29180f78a191071d310149c725b2
tonymorony <tlysakov@gmail.com>
2019-12-22 17:20:17 +0700

    pybadges

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import badge_lib
+import time
 
 
 def main():
     while True:
-        badge_lib.update_last_nota_status(badge_lib.kmd_notarized_coins)
+        # badge_lib.update_last_nota_status(badge_lib.kmd_notarized_coins)
         for coin_data in badge_lib.kmd_notarized_coins:
-            badge_lib.create_coin_badge(coin_data)
-        # TODO: just add sleep there to re-draw badges periodically
-        break
+            badge_lib.coin_dpow_badge(coin_data)
+        time.sleep(360)
 
 
 if __name__ == '__main__':
