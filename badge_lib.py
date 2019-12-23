daac19a36eabecc0e0243936e8c31254e4bd73c3
Anton Lysakov <tlysakov@gmail.com>
2019-12-23 23:24:48 +0700

    pybadges patch

--- badge_lib.py
+++ badge_lib.py
@@ -409,21 +409,29 @@ def create_coin_badge(coin_data):
     result.save(coin_data["name"]+"_badge.png")
 
 
-def coin_dpow_badge(coin_data):
-    coin_status_dict = json.loads(requests.get("https://komodostats.com/api/notary/coin.json",
-                                               params={"coinName": coin_data["name"]}).content)
-    minutes_since_last_nota = (int(time.time()) - coin_status_dict["lastnotarization"]) / 60
-    coin_data["minutes_since_nota"] = round(minutes_since_last_nota)
-    if coin_data["minutes_since_nota"] < 144:
-        coin_dpow_status = "active"
-        status_color = "green"
-    elif 720 < coin_data["minutes_since_nota"] < 1440:
-        coin_dpow_status = "irregular"
-        status_color = "yellow"
-    else:
-        coin_dpow_status = "inactive"
-        status_color = "red"
-    dpow_status_badge = badge(left_text='dPOW status', right_text=coin_dpow_status, right_color=status_color)
-    with open(coin_data["name"]+"_badge.svg", 'w') as f:
-        f.write(dpow_status_badge)
+def coin_dpow_badge(notarized_coins_list):
+    coins_notarization_info = json.loads(requests.get("https://komodostats.com/api/notary/summary.json").content)
+    for coin_data in notarized_coins_list:
+        print("Creating badge for " + coin_data["name"])
+        for coin_fetched_data in coins_notarization_info:
+           if coin_data["name"] == coin_fetched_data["ac_name"]:
+               try:
+                   minutes_since_last_nota = (int(time.time()) - coin_fetched_data["lastnotarization"]) / 60
+                   coin_data["minutes_since_nota"] = round(minutes_since_last_nota)
+               except Exception as e:
+                   print(e)
+                   print(coin_data)
+                   print(coin_fetched_data)
+               if int(coin_data["minutes_since_nota"]) < 144:
+                   coin_dpow_status = "active"
+                   status_color = "green"
+               elif 720 < int(coin_data["minutes_since_nota"]) < 1440:
+                   coin_dpow_status = "irregular"
+                   status_color = "yellow"
+               else:
+                   coin_dpow_status = "inactive"
+                   status_color = "red"
+               dpow_status_badge = badge(left_text='dPOW status', right_text=coin_dpow_status, right_color=status_color)
+               with open("/var/www/html/svg/"+coin_data["name"]+"_badge.svg", 'w') as f:
+                   f.write(dpow_status_badge)
 
--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import time
 
 def main():
     while True:
-        # badge_lib.update_last_nota_status(badge_lib.kmd_notarized_coins)
-        for coin_data in badge_lib.kmd_notarized_coins:
-            badge_lib.coin_dpow_badge(coin_data)
+        badge_lib.coin_dpow_badge(badge_lib.kmd_notarized_coins)
         time.sleep(360)
 
 
